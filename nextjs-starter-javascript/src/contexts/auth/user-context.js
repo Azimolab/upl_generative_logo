4a298c256bdcadcdcffeef8164f703d5c55e389e
Eduardo Orsi <eduardoorsi.source@gmail.com>
2024-03-12 16:43:06 -0300

    First Commit

---
+++ nextjs-starter-javascript/src/contexts/auth/user-context.js
@@ -0,0 +1,26 @@
+import { config } from '@/config';
+import { AuthStrategy } from '@/lib/auth/strategy';
+
+import { UserContext as CustomUserContext, UserProvider as CustomUserProvider } from './custom/user-context';
+// import { UserContext as FirebaseUserContext, UserProvider as FirebaseUserProvider } from './firebase/user-context';
+
+// eslint-disable-next-line import/no-mutable-exports -- Export based on config
+let UserProvider;
+
+// eslint-disable-next-line import/no-mutable-exports -- Export based on config
+let UserContext;
+
+switch (config.auth.strategy) {
+   case AuthStrategy.CUSTOM:
+     UserContext = CustomUserContext;
+     UserProvider = CustomUserProvider;
+     break;
+  // case AuthStrategy.FIREBASE:
+  //   UserContext = FirebaseUserContext;
+  //   UserProvider = FirebaseUserProvider;
+  //   break;
+  default:
+    throw new Error('Invalid auth strategy');
+}
+
+export { UserProvider, UserContext };
